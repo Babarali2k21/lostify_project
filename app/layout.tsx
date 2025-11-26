4bd2d3ccf7b3d741340fbd946f3b1d4cc565235d
Muhammad Hamza Azeem <muhammad.azeem@student.uibk.ac.at>
2025-11-26 10:30:23 +0100

    add toast component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Toaster } from 'sonner';
 
 export const metadata: Metadata = {
   title: "Lostify",
@@ -15,6 +16,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+        <Toaster richColors position="top-right" />
       </body>
     </html>
   );
